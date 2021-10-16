b943fca9d45a2411ee7ad439fe4a0995424d853f
nguyenminhhp2003 <nguyenminhhp2003@gmail.com>
2021-10-16 20:29:37 +0700

    n

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ session_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>php.php</title>
+  <title>login</title>
 </head>
 <body>
 
@@ -75,6 +75,7 @@ a:hover, a:active ,input[type=submit]:hover{
 
 <!-- style------------------------- -->
 
+
 <?php
 
 $servername = "localhost";
@@ -88,14 +89,12 @@ echo "<br>";
 
 ?>
 
-
-
 <div class= "logindiv">
   <form class="login" action="" method="post">
   Username: <input type="text" name="username" > <br>
   Password: <input type="password" name="password" >
   <br>
-  <input id = "sbm" type="submit" name= "login" value="login">
+  <input id = "sbm" type="submit" name= "log" value="login">
 <br> 
 <div id = "nouser" ></div>
 <br>
@@ -105,9 +104,12 @@ echo "<br>";
 </form>
 <br>
 </div>
+
+
 <?php
 
-if (isset($_POST["login"])) {
+if (isset($_POST["log"])) {
+  
   $name = $_POST["username"];
   $passwd = $_POST["password"];
   
@@ -118,13 +120,13 @@ if (isset($_POST["login"])) {
   $dbfetch =  mysqli_fetch_array($resul,MYSQLI_ASSOC);
   $row = mysqli_num_rows($resul);
   $count = 0;
-if ($row == 1) {
+  if ($row == 1) {
     echo "<script>window.location.href='pentesting.php'</script>";
     $_SESSION["name"] = $dbfetch["Name"];
     
   
   }else{
-    echo "<script>document.getElementById('nouser').innerHTML='No valid user SUCKER. GO try again LOSERRRRR!!';</script>";
+    echo "<script>document.getElementById('nouser').innerHTML='No valid user found, SUCKER. GO try again LOSERRRRR!!';</script>";
   }
   
 }
